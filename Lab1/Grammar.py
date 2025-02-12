b6fa6ca38684ddc6284cb2468229edeaab68ea86
Mihaela Catan <mihaelacatan7@gmail.com>
2025-02-13 00:01:06 +0200

    implement grammar class

---
+++ Lab1/Grammar.py
@@ -0,0 +1,26 @@
+import random
+
+class Grammar:
+    def __init__(self, non_terminals, terminals, productions, start_symbol):
+        self.non_terminals = non_terminals
+        self.terminals = terminals
+        self.productions = productions
+        self.start_symbol = start_symbol
+
+    def generate_string(self):
+        result = random.choice(self.productions[self.start_symbol])
+        while any(non_terminal in result for non_terminal in self.non_terminals):
+            final_result = ""
+            for letter in result:
+                if letter in self.non_terminals:
+                    final_result += random.choice(self.productions[letter])
+                else:
+                    final_result += letter
+            result = final_result
+        return result
+
+    def generate_strings(self, number_of_strings):
+        strings = []
+        for _ in range(number_of_strings):
+            strings.append(self.generate_string())
+        return strings
--- Lab1/main.py
+++ Lab1/main.py
@@ -1,5 +1,19 @@
+from Lab1.Grammar import Grammar
+
+
 def main():
-    print("hey there")
+    non_terminals = ["S", "I", "J", "K"]
+    terminals = ["a", "b", "c", "e", "n", "f", "m"]
+    productions = {
+        'S': ['cI'],
+        'I': ['bJ', 'fI', 'e', 'eK'],
+        'J': ['nJ', 'cS'],
+        'K': ['nK', 'm']
+    }
+    start_symbol = "S"
+
+    grammar = Grammar(non_terminals, terminals, productions, start_symbol)
+    print(grammar.generate_strings(5))
 
 if __name__=="__main__":
     main()
