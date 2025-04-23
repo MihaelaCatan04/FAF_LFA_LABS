82e6fab0fe04910feba034e439381b12f1768c39
Mihaela Catan <mihaelacatan7@gmail.com>
2025-04-24 00:35:43 +0300

    Create Node Class structures for each AST structure in the AST

---
+++ Lab6/src/StringNode.py
@@ -0,0 +1,5 @@
+from Lab6.src.LiteralNode import LiteralNode
+
+
+class StringNode(LiteralNode):
+    pass
---
+++ Lab6/src/UnitNode.py
@@ -0,0 +1,5 @@
+from Lab6.src.LiteralNode import LiteralNode
+
+
+class UnitNode(LiteralNode):
+    pass
